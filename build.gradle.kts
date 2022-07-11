20aedeaae8d4e1781ce18b4aa00ee368469347b7
Raphael Tarita <raphael.tarita@gmail.com>
2022-07-11 10:17:58 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "com.rtarita"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api("com.warrenstrange:googleauth:1.5.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
