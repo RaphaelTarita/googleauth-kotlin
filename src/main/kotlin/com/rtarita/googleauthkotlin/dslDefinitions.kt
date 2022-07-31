e0a778cb088470f5ffc40b7998cc23fb12f17186
Azratosh <azratosh@gmail.com>
2022-07-31 21:53:12 +0200

    Add DSL definition for KGoogleAuthenticatorKeyBuilder

--- src/main/kotlin/com/rtarita/googleauthkotlin/dslDefinitions.kt
+++ src/main/kotlin/com/rtarita/googleauthkotlin/dslDefinitions.kt
@@ -2,9 +2,16 @@ package com.rtarita.googleauthkotlin
 
 import com.warrenstrange.googleauth.GoogleAuthenticator
 import com.warrenstrange.googleauth.GoogleAuthenticatorConfig
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey
 
 public inline fun googleAuthenticator(builderAction: KGoogleAuthenticatorConfigBuilder.() -> Unit): GoogleAuthenticator {
     val configBuilder = KGoogleAuthenticatorConfigBuilder(GoogleAuthenticatorConfig.GoogleAuthenticatorConfigBuilder())
     configBuilder.builderAction()
     return GoogleAuthenticator(configBuilder.build())
+}
+
+public inline fun googleAuthenticatorKey(builderAction: KGoogleAuthenticatorKeyBuilder.() -> Unit, key: String): GoogleAuthenticatorKey{
+    val keyBuilder = KGoogleAuthenticatorKeyBuilder(GoogleAuthenticatorKey.Builder(key))
+    keyBuilder.builderAction()
+    return keyBuilder.build()
 }
